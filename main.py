6542d70012fe804099bc8df0a92953b0ebf0f893
marksarapornvisit <marksarapornvisit@gmail.com>
2017-04-26 14:37:08 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,19 +8,23 @@ import numpy as np
 import imutils
 import time
 import cv2
+import serial
 
 import lanedetector as lane
 import obstracleavoidance as obst
+import car_angle_goal as carang
+import datatoarduino as arduino
 
 # import os
 
 
-fvs = FileVideoStream("videos/GOPR0204.avi").start()
+fvs = FileVideoStream("videos/GOPR0205_3.avi").start()
 
 # start the FPS timer
 fps = FPS().start()
 time.sleep(1.0)
 i = 0
+
 # loop over frames from the video file stream
 while fvs.more():
     # grab the frame from the threaded video file stream, resize
@@ -42,10 +46,30 @@ while fvs.more():
         pathAvaliable = obst.main(perspective)
 
         # detect and draw 
-        for i in range(3,7):
+        for i in range(3,4):
             region, thresh, position = lane.detect(perspective, [[0, 1], [i/10, i/10+0.05]])
             lane.drawpos(region, position)
+
+            #mark edit 23 apr 2017
+            if i == 3:
+                upposition = position
+            if i == 4:
+                loposition = position
+            #mark end
+
+
+            #print (position,len(position))
+
         # main algorithm ends here
+        #mark edit 23 apr 2017
+        cv2.line(region,(240,0),(240,60),(255,0,0),5)
+        centercal,centershift = carang.carcenterpos(carang.choselane(position))
+        cv2.line(region,(centercal,0),(centercal,60),(0,0,255),5)
+        print (centershift)
+        arduino.send(centershift)
+
+
+
 
         # display the size of the queue on the frame
         cv2.putText(gray, "Queue Size: {}".format(fvs.Q.qsize()), (10, 30),
@@ -53,12 +77,12 @@ while fvs.more():
 
         # show the frame and update the FPS counter
         H = cv2.cvtColor(perspective, cv2.COLOR_BGR2HSV)[:,:,0]
-        cv2.imshow("pathAvaliable", pathAvaliable)
+        #cv2.imshow("pathAvaliable", pathAvaliable)
         cv2.imshow("real", gray)
         cv2.imshow("Perspective", perspective)
-
         if cv2.waitKey(1) == ord('q'):
             break
+        #cv2.waitKey(0)
     else:
         pass
     time.sleep(0.05)
